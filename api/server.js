0d92c9729d92bf4ef4e1b5f737537a8e13829245
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-11-29 15:57:31 -0600

    Initialized server, created get request for getting all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,33 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
+// Imports above
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+// Get all users
+
+server.get('/api/users', (req,res) => {
+    Users.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'unable to retrieve users',
+                err: err.message
+            })
+        })
+})
+
+// Get user by Id
+
+// Post new user
+
+// Put (update) user by id
+
+// Delete by id
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
